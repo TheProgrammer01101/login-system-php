10d31564e7343e3d59d9653d7509cd58f63ef6d8
Andre Tausche <65219501+Berzerk3000@users.noreply.github.com>
2020-11-29 10:44:17 +0100

    comment with additional task
    same as before - instead or printing or echoing create a template and insert it

--- includes/scripts/signUp.inc.php
+++ includes/scripts/signUp.inc.php
@@ -20,6 +20,7 @@ if (emailExists($pdo, $email) == true) {
   $query_str = "INSERT INTO system_users(name, email, username, password) VALUES(:fullName, :email, :username, :password)";
   $stmt = $pdo->prepare($query_str);
   $stmt->execute(['fullName' => $fullName, 'email' => $email, 'username' => $username, 'password' => $hashedpwd]);
+  # instead of print let's add the template for the content when signed in
   print "signed up!";
   
 }

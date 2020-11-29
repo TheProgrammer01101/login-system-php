95e4172c4cc2f5a2956c8589d3d1e950e0a20072
Barzi Jaf <barzi4greatness@gmail.com>
2020-11-29 16:15:10 +0100

    made controller more dynamic

--- includes/scripts/controller.php
+++ includes/scripts/controller.php
@@ -1,16 +1,10 @@
 <?php
-  if($_GET['page'] == "signin"){
+
+  if(isset($_GET['page'])) {
     include_once ("includes/templates/" . $_GET['page'] . ".php");
   }
 
-  else if($_GET['page'] == "signup"){
-    include_once ("includes/templates/" . $_GET['page'] . ".php");
-  }
-
-# it's better to check if page variable is set.. replace line 2 to 8 
-# if(isset($_GET['page'])) include_once ("includes/templates/" . $_GET['page'] . ".php");
-
-  else{
+  else {
     include_once "includes/templates/home.php";
   }
 ?>
